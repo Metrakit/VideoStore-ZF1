e1873c5070a7805fd3f651a5f98265fa79191de3
metrakit <metrakit@github.com>
2014-10-07 12:07:27 +0200

    Add assets and Zend architecture

---
+++ src/application/Bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+    // Permet de surchager la configuration du fichier .ini
+   /* protected function _initToto()
+    {
+        
+    }*/
+}
---
+++ src/application/plugins/Test.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 
+ * @author Jordane
+ * Test d'un Plugin
+ * /!\ Il faut absolutement que la classe commence par "Plugin_*" pour l'autoloading
+ *
+ */
+class Plugin_Test extends Zend_Controller_Plugin_Abstract
+{
+    /**
+     * Fonction éxecutée après le routage !
+     * Il faut mettre le typage de l'objet dans la fonction en paramètre
+     * @see Zend_Controller_Plugin_Abstract::routeShutdown()
+     */
+    public function routeShutdown(Zend_Controller_Request_Abstract $request)
+    {
+        if ($request->getActionName() == 'index') {
+            echo '<p>Action index détectée</p>';
+        }
+    }
+}
